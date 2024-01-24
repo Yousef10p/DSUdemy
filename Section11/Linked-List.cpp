6f99244a687e429c2b933175db6b2d206f9de730
yousef <Yousef.alogiley@gmail.com>
2024-01-24 14:39:30 +0300

    Linked List file improved with more functions

--- Section11/Linked-List.cpp
+++ Section11/Linked-List.cpp
@@ -1,15 +1,16 @@
-//linked-list
-#include<iostream>
+// linked-list
+#include <iostream>
 using namespace std;
 
 class node
 {
-    private:
-        int data;
-        node* next;
-    public:    
+private:
+    int data;
+    node *next;
+
+public:
     node(int x)
-    {   
+    {
         data = x;
         next = NULL;
     }
@@ -22,7 +23,7 @@ class node
         return data;
     }
 
-    node* getnext()
+    node *getnext()
     {
         return next;
     }
@@ -30,18 +31,18 @@ class node
     void setnext(node *ptr)
     {
         next = ptr;
-    }           
+    }
 };
 
 class list
 {
-    private:
-        node *head;
-    public:
+private:
+    node *head;
 
+public:
     list()
     {
-        head = NULL;//emptey intially
+        head = NULL; // emptey intially
     }
     void create()
     {
@@ -57,25 +58,75 @@ class list
         cout << "Enter data: ";
         cin >> input;
         node *p = new node(input);
-        p->setnext(head);//p->next = head;
+        p->setnext(head); // p->next = head;
         head = p;
     }
     void removeElement()
     {
         node *ptr = head;
-        head = head->getnext();//head = head->next;
-        ptr->setnext(NULL);//ptr->next = NULL;
+        head = head->getnext(); // head = head->next;
+        ptr->setnext(NULL);     // ptr->next = NULL;
+    }
+
+    void removeElementByKey()
+    {
+        int key;
+        cout << "Enter data value: ";
+        cin >> key;
+
+        node *ptr = head;
+        node *prev;
+        if (ptr->getdata() == key)
+            removeElement();
+        else
+            while (ptr->getnext() != NULL)
+            {
+                prev = ptr;
+                ptr = ptr->getnext();
+                if (ptr->getdata() == key)
+                {
+                    prev->setnext(ptr->getnext());
+                    ptr->setnext(NULL);
+                }
+            }
     }
     void display()
     {
         node *ptr = head;
-        while(ptr != NULL)
-            {
-                cout << ptr->getdata();
-                if(ptr->getnext())cout <<',';
-                ptr = ptr->getnext();
-            }
+        while (ptr != NULL)
+        {
+            cout << ptr->getdata() << ' ';
+            ptr = ptr->getnext();
+        }
         cout << endl;
+    }
+
+    void reverseDisplay()
+    {
+        recursiveDisplay(head);
+        cout << endl;
+    }
+
+private:
+    void recursiveDisplay(node *h)
+    {
+        if (h)
+        {
+            recursiveDisplay(h->getnext());
+            cout << h->getdata() << ' ';
+        }
+    }
+public:
+    void countNodes()
+    {
+        int count = 0;
+        node *ptr = head;
+        while(ptr != NULL)
+        {
+            count++;
+            ptr = ptr->getnext();
+        }
+        cout << "Number of nodes: " << count << endl;
     }    
 };
 
@@ -90,30 +141,48 @@ int main()
         cout << "1 To create Linked-List" << endl;
         cout << "2 To add new element to the list" << endl;
         cout << "3 To remove an element from the list" << endl;
-        cout << "4 To display the list" << endl;
+        cout << "4 TO remove an element from the list by Key value" << endl;
+        cout << "5 To display the list" << endl;
+        cout << "6 TO display reverse " << endl;
+        cout << "7 To count number of nodes(Elements)" << endl;
         cout << "0 To terminate the program" << endl;
         cout << "Choice: ";
         cin >> choice;
 
-            switch (choice)
-            {
-                case 1:
-                    mylist.create();
-                    break; 
-                case 2:
-                    mylist.addNewElement();
-                    break;
-                case 3:
-                    mylist.removeElement();
-                    break;
-                case 4: mylist.display();
-                    break;
-                default : break;                
-            }
-            if(!choice)break;
+        switch (choice)
+        {
+        case 0:
+            break;
+        case 1:
+            mylist.create();
+            break;
+        case 2:
+            mylist.addNewElement();
+            break;
+        case 3:
+            mylist.removeElement();
+            break;
+        case 4:
+            mylist.removeElementByKey();
+            break;
+        case 5:
+            mylist.display();
+            break;
+        case 6:
+            mylist.reverseDisplay();
+            break;
+        case 7:
+            mylist.countNodes();
+            break;    
+        default:
+            cout << "Number entered not correct" << endl;
+            break;
+        }
+        if (!choice)
+            break;
 
     } while (true);
-    
+
     cout << endl;
     cout << endl;
     cout << endl;
